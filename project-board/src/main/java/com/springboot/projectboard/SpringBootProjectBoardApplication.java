c27924d4154def36fc5d7b8ef4ae1201d9abc049
LeeDuho <dl6874@gmail.com>
2023-03-27 17:25:00 +0900

    #5-스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    *스프링 웹
    *엑츄에이터
    *데브툴즈
    *롬복

---
+++ project-board/src/main/java/com/springboot/projectboard/SpringBootProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectBoardApplication.class, args);
+	}
+
+}
---
+++ project-board/src/test/java/com/springboot/projectboard/SpringBootProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
