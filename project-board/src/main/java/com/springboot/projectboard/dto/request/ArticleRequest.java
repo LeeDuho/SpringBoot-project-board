bf1a8afb62e4ac3aad65919cc395131f2120f4fe
LeeDuho <dl6874@gmail.com>
2023-04-28 23:27:02 +0900

    #17 - 서비스 로직 테스트 코드 업데이트, dto 재설

---
+++ project-board/src/main/java/com/springboot/projectboard/dto/request/ArticleRequest.java
@@ -0,0 +1,26 @@
+package com.springboot.projectboard.dto.request;
+
+import com.springboot.projectboard.dto.ArticleDto;
+import com.springboot.projectboard.dto.UserAccountDto;
+
+public record ArticleRequest(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleRequest of(String title, String content, String hashtag) {
+        return new ArticleRequest(title, content, hashtag);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtag
+        );
+    }
+
+
+}
